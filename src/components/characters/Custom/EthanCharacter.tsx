b5a947f0b230f8a7031d594fc6943a8c141d97c9
ehammons11 <ehammons@mit.edu>
2026-03-29 23:57:34 -0700

    Character Audio Refactor

--- src/components/characters/Custom/EthanCharacter.tsx
+++ src/components/characters/Custom/EthanCharacter.tsx
@@ -1,12 +1,9 @@
 import { type ThreeElements, useFrame } from "@react-three/fiber";
 import { OctahedronCharacter } from "@/components/characters/OctahedronCharacter";
 import { Float } from "@react-three/drei";
-import { useCallback, useEffect, useRef } from "react";
+import { useRef } from "react";
 import { type Group } from "three";
-import {
-  SpatialAudioSource,
-  type SpatialAudioSourceRef,
-} from "@/components/audio/SpatialAudioSource";
+import { SpatialAudioSource } from "@/components/audio/SpatialAudioSource";
 import { useConversationContext } from "@/hooks/useConversationContext";
 
 const ROTATION_SPEED = 0.3;
@@ -15,22 +12,9 @@ const ROTATION_SPEED = 0.3;
  * Custom character for Ethan's Odyssey.
  */
 export function EthanCharacter(props: ThreeElements["group"]) {
-  const { setOnAudio } = useConversationContext();
+  const { agentMediaStream } = useConversationContext();
 
   const groupRef = useRef<Group>(null);
-  const audioRef = useRef<SpatialAudioSourceRef>(null);
-
-  const handleAudio = useCallback((base64Audio: string) => {
-    audioRef.current?.enqueueAudio(base64Audio);
-  }, []);
-
-  // Connect the conversation audio to the SpatialAudioSource.
-  useEffect(() => {
-    setOnAudio(handleAudio);
-    return () => {
-      setOnAudio(null);
-    };
-  }, [handleAudio, setOnAudio]);
 
   useFrame((_, delta) => {
     if (!groupRef.current) return;
@@ -43,7 +27,7 @@ export function EthanCharacter(props: ThreeElements["group"]) {
     <group ref={groupRef} {...props}>
       <Float floatIntensity={2} rotationIntensity={2}>
         <OctahedronCharacter color="#641547" />
-        <SpatialAudioSource ref={audioRef} />
+        <SpatialAudioSource mediaStream={agentMediaStream} />
         {/* <mesh geometry={ORB_GEOMETRY} material={ORB_MATERIAL} renderOrder={2} /> */}
       </Float>
     </group>
