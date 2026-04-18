28f620afd54cf64a40628da448018b3717dadd09
ehammons11 <ehammons@mit.edu>
2026-04-18 12:58:08 -0700

    adr

--- src/components/characters/components/ButterflyWing.tsx
+++ src/components/characters/components/ButterflyWing.tsx
@@ -1,7 +1,7 @@
 import { type ThreeElements, useFrame } from "@react-three/fiber";
 import { type Signal } from "@preact/signals-react";
 import { ShaderMaterial, TextureLoader, Uniform, DoubleSide } from "three";
-import { useState } from "react";
+import { useMemo } from "react";
 
 import vertexShader from "@/shaders/butterfly/vertex.glsl?raw";
 import fragmentShader from "@/shaders/butterfly/fragment.glsl?raw";
@@ -16,21 +16,27 @@ type ButterflyWingProps = ThreeElements["group"] & {
   flapSpeed: Signal<number>;
 };
 
+/**
+ * Renders a set of flapping butterfly wings using a custom shader.
+ *
+ * @param flapSpeed A signal controlling wing flapping speed.
+ */
 export function ButterflyWing({ flapSpeed, ...props }: ButterflyWingProps) {
-  const [material] = useState(
+  const material = useMemo(
     () =>
       new ShaderMaterial({
         vertexShader,
         fragmentShader,
         uniforms: {
           uTime: new Uniform(0),
-          uFlapSpeed: new Uniform(flapSpeed.value),
+          uFlapSpeed: new Uniform(flapSpeed.peek()),
           uWingTexture: new Uniform(wingTexture),
           uJewelGradient: new Uniform(jewelGradient),
           uJewelNoise: new Uniform(jewelNoise),
         },
         side: DoubleSide,
       }),
+    [flapSpeed],
   );
 
   useFrame((_, delta) => {
