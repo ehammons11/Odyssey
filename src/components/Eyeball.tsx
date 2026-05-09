b5533fc3f54c2451680420ebf45d854d115c1d3d
ehammons11 <ehammons@mit.edu>
2026-05-09 00:17:33 -0500

    Splat and Eye Optimization

--- src/components/Eyeball.tsx
+++ src/components/Eyeball.tsx
@@ -5,12 +5,12 @@ import { Mesh, MeshStandardMaterial } from "three";
 
 type GLTFResult = GLTF & {
   nodes: {
-    Eye_Eye_0: Mesh;
-    Eye_Iris_0: Mesh;
+    Object_2: Mesh;
+    Object_3: Mesh;
   };
   materials: {
-    material: MeshStandardMaterial;
-    Iris: MeshStandardMaterial;
+    ["Material.001"]: MeshStandardMaterial;
+    ["Material.002"]: MeshStandardMaterial;
   };
 };
 
@@ -18,35 +18,27 @@ type GLTFResult = GLTF & {
  * Eyeball component that loads and displays a low-poly eyeball model.
  */
 export function Eyeball(props: ThreeElements["group"]) {
-  const { nodes, materials } = useGLTF(
-    "/human_eye.glb",
-  ) as unknown as GLTFResult;
+  const { nodes, materials } = useGLTF("/eye.glb") as unknown as GLTFResult;
   return (
     <group {...props} dispose={null}>
-      <group rotation={[-1.807, 0, 0]}>
-        <group rotation={[Math.PI / 1.8, 0, 0]} scale={0.01}>
-          <group
-            position={[0, 0, 16.654]}
-            rotation={[-Math.PI / 2, 0, 0]}
-            scale={100}
-          >
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={nodes.Eye_Eye_0.geometry}
-              material={materials.material}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={nodes.Eye_Iris_0.geometry}
-              material={materials.Iris}
-            />
-          </group>
+      <group {...props} dispose={null}>
+        <group rotation={[-Math.PI / 2, 0, 0]}>
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_2.geometry}
+            material={materials["Material.001"]}
+          />
+          <mesh
+            castShadow
+            receiveShadow
+            geometry={nodes.Object_3.geometry}
+            material={materials["Material.002"]}
+          />
         </group>
       </group>
     </group>
   );
 }
 
-useGLTF.preload("/human_eye.glb");
+useGLTF.preload("/eye.glb");
